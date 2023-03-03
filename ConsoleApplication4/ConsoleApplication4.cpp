cf045c8fcc619ce3ce1f8ee5cfa81cfa817b2ed7
1sadssd <den20062345@gmail.com>
2023-03-03 20:40:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() 
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    string s;
+    int m, n;
+
+    cout << "Введіть рядок: ";
+    getline(cin, s);
+    cout << "Введіть m та n: ";
+    cin >> m >> n;  
+
+    s.erase(m - 1, n - m + 1);
+    cout << "Результат: " << s << endl;
+}
+
+
+     
+
+
+
+
+
+
+
